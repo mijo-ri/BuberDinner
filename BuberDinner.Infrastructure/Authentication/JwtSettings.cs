a9ed07448ba742c73df5ff8e2d040a0328f77ba7
Michael John Rieser <michi.rieser@gmail.com>
2025-09-14 19:29:38 +0100

    Add authentication and authorization

--- BuberDinner.Infrastructure/Authentication/JwtSettings.cs
+++ BuberDinner.Infrastructure/Authentication/JwtSettings.cs
@@ -3,8 +3,8 @@ namespace BuberDinner.Infrastructure.Authentication;
 public class JwtSettings
 {
     public const string SectionName = "JwtSettings";
-    public required string Secret { get; init; }
+    public string Secret { get; init; } = null!;
     public int ExpiryMinutes { get; init; }
-    public required string Issuer { get; init; }
-    public required string Audience { get; init; }
+    public string Issuer { get; init; } = null!;
+    public string Audience { get; init; } = null!;
 }
